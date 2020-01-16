073fc4b3af48610d76d0a3e501fd2751e22c10d3
JackRyan1989 <jack.jackryan@gmail.com>
2020-01-16 15:25:48 -0500

    Updates

---
+++ precache-manifest.a5e2db52ca7da07b9e7cd6698be6e1b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1f48ac49e425d946fcf1467e2b7599",
+    "url": "/clickyMemoryGame/clicky-game/index.html"
+  },
+  {
+    "revision": "192b436517dfd265aa91",
+    "url": "/clickyMemoryGame/clicky-game/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "53c6882768787067929a",
+    "url": "/clickyMemoryGame/clicky-game/static/css/main.cb0d758f.chunk.css"
+  },
+  {
+    "revision": "192b436517dfd265aa91",
+    "url": "/clickyMemoryGame/clicky-game/static/js/2.c063bf78.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/clickyMemoryGame/clicky-game/static/js/2.c063bf78.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53c6882768787067929a",
+    "url": "/clickyMemoryGame/clicky-game/static/js/main.7be71714.chunk.js"
+  },
+  {
+    "revision": "1b07d6c18a3caf892a4c",
+    "url": "/clickyMemoryGame/clicky-game/static/js/runtime-main.1a19f0bd.js"
+  },
+  {
+    "revision": "36daf958ea2000b9772003ed27a3a620",
+    "url": "/clickyMemoryGame/clicky-game/static/media/ren-and-stimpy-background-1.36daf958.jpg"
+  }
+]);
