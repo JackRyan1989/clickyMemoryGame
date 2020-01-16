f335d2391064326152b534b2d29450035f0cfbad
JackRyan1989 <jack.jackryan@gmail.com>
2020-01-16 15:33:36 -0500

    Updates

---
+++ precache-manifest.524955b2319e5feef9e8ccc5513f549d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae277dfa4b010079be3ab16b77387580",
+    "url": "/clickyMemoryGame/index.html"
+  },
+  {
+    "revision": "192b436517dfd265aa91",
+    "url": "/clickyMemoryGame/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1b9a65bb393c47a084a0",
+    "url": "/clickyMemoryGame/static/css/main.fc016967.chunk.css"
+  },
+  {
+    "revision": "192b436517dfd265aa91",
+    "url": "/clickyMemoryGame/static/js/2.c063bf78.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/clickyMemoryGame/static/js/2.c063bf78.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b9a65bb393c47a084a0",
+    "url": "/clickyMemoryGame/static/js/main.465ff8df.chunk.js"
+  },
+  {
+    "revision": "12016baacd0c178590af",
+    "url": "/clickyMemoryGame/static/js/runtime-main.b68bf74d.js"
+  },
+  {
+    "revision": "36daf958ea2000b9772003ed27a3a620",
+    "url": "/clickyMemoryGame/static/media/ren-and-stimpy-background-1.36daf958.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61d4d84f5e5df39ac32d2e2a69ddf9fd.js"
+  "/clickyMemoryGame/precache-manifest.524955b2319e5feef9e8ccc5513f549d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clickyMemoryGame/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
